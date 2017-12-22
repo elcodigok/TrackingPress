5e933612ecb81bc6e10e207b5cc8a47d5ec81287
Daniel Maldonado <daniel_5502@yahoo.com.ar>
2017-12-21 21:58:56 -0300

    Add class OsInfo()

--- trackingpress.py
+++ trackingpress.py
@@ -1,3 +1,25 @@
 #!/usr/bin/python3
 
+import os
+
+class OsInfo:
+    def getSysname(self):
+        return os.uname().sysname
+    
+    def getVersion(self):
+        return os.uname().version
+    
+    def getRelease(self):
+        return os.uname().release
+    
+    def getMachine(self):
+        return os.uname().machine
+    
+    def getNodename(self):
+        return os.uname().nodename
+
 print ("Hello TrackingPress")
+system = OsInfo()
+print (system.getSysname())
+print (system.getVersion())
+print (system.getRelease())
